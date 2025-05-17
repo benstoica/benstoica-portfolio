19c82acd7a819f7db7be8160f8e562122a6ab8fb
Ben Stoica <bennystoica@gmail.com>
2025-05-16 21:15:43 -0400

    home section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import LoadingScreen from "./components/LoadingScreen";
 import Navbar from "./components/Navbar";
 import MobileMenu from "./components/MobileMenu";
+import Home from "./components/sections/Home";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
@@ -19,6 +20,7 @@ function App() {
         } bg-black text-gray-100`}>
         <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
         <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        <Home />
       </div>
     </>
   );
