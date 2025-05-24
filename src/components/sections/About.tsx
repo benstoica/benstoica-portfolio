7abb888cc49c92c69de3bdc962d9fabe00f12104
Ben Stoica <bennystoica@gmail.com>
2025-05-23 21:02:48 -0400

    update layout, add more projects, add icons

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,7 +1,27 @@
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 import RevealOnScroll from "../RevealOnScroll";
 
-const frontendSkills = ["React", "Next.JS", "TypeScript", "Tailwind"];
-const backendSkills = ["Python", "NodeJS", "C#"];
+const skills = [
+  "Automation",
+  "C#",
+  "CSS",
+  "Figma",
+  "HTML",
+  "Jest",
+  "Next.JS",
+  "NodeJS",
+  "Playwright",
+  "Postman",
+  "Python",
+  "React",
+  "React Testing Library",
+  "Responsive Design",
+  "Selenium",
+  "Storybook",
+  "Tailwind",
+  "Testing",
+  "TypeScript",
+];
 
 const About = () => {
   return (
@@ -29,31 +49,33 @@ const About = () => {
               employers.
             </p>
 
-            <div className="grid grid-cols-2 md:grid-cols-2 gap-6">
-              <div className="rounded-xl p-6">
-                <h3 className="text-xl font-bold mb-4">Frontend</h3>
-                <div className="flex flex-wrap gap-2">
-                  {frontendSkills.map((skill) => (
-                    <span
-                      key={skill}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                      {skill}
-                    </span>
-                  ))}
-                </div>
-              </div>
+            <div className="flex justify-center mb-6 gap-6">
+              <a
+                className="icon"
+                target="_blank"
+                href="https://github.com/benstoica"
+                rel="noreferrer">
+                <BsGithub className="lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:cursor-pointer text-white/90 hover:text-white" />
+              </a>
+              <a
+                className="icon"
+                target="_blank"
+                href="https://github.com/benstoica"
+                rel="noreferrer">
+                <BsLinkedin className="lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:cursor-pointer text-blue-500 hover:text-white" />
+              </a>
+            </div>
 
-              <div className="rounded-xl p-6">
-                <h3 className="text-xl font-bold mb-4">Backend</h3>
-                <div className="flex flex-wrap gap-2">
-                  {backendSkills.map((skill) => (
-                    <span
-                      key={skill}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                      {skill}
-                    </span>
-                  ))}
-                </div>
+            <div className="rounded-xl p-6">
+              <h3 className="text-xl font-bold mb-4">Skills</h3>
+              <div className="flex flex-wrap gap-2">
+                {skills.map((skill) => (
+                  <span
+                    key={skill}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                    {skill}
+                  </span>
+                ))}
               </div>
             </div>
           </div>
@@ -63,28 +85,66 @@ const About = () => {
               <h3 className="text-xl font-bold mb-4">Education</h3>
               <ul className="list-disc list-inside text-gray-300 space-y-2">
                 <li>
-                  <strong>
-                    A.S. in Network Administration - DeVry University
-                  </strong>
+                  Full Stack Web Development Certification – Free Code Camp
                 </li>
-                <li>Relevant Coursework: Web Development</li>
+                <li>A.S. in Network Administration - DeVry University</li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/benstoica/details/certifications/1635546100850/single-media-viewer/?profileId=ACoAABImNZQB1701Ks7Xu0629agzUEcyuD6ush4"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-blue-500 hover:underline">
+                    Change Management – Oakland Community College{" "}
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/benstoica/certs/blob/master/Advanced%20HTML5%20%26%20CSS3%20Cert.pdf"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-blue-500 hover:underline">
+                    Advanced HTML5 & CSS3 Specialist – CIW Advisory Council
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/benstoica/certs/blob/master/CIW%20User%20Interface%20Design%20Certificate.pdf"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-blue-500 hover:underline">
+                    CIW User Interface Designer – CIW Advisory Council
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.credly.com/badges/7ef5f327-8ab0-46c6-819e-dc32e949baa1/linked_in_profile"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-blue-500 hover:underline">
+                    CompTIA Project+
+                  </a>
+                </li>
               </ul>
             </div>
             <div className="p-6 rounded-xl border-white/10 border">
               <h3 className="text-xl font-bold mb-4">Work Experience</h3>
               <div className="space-y-4 text-gray-300 ">
                 <div>
-                  <h4 className="font-semibold">
-                    RPM Freight Systems - Senior Software Engineer
-                  </h4>
-                  <p>Developed and maintined internal applications.</p>
+                  <h4 className="font-semibold">RPM Freight Systems</h4>
+                  <p className="text-sm">Senior Software Engineer</p>
                 </div>
 
                 <div>
-                  <h4 className="font-semibold">
-                    United Wholesale Mortgage - UI Developer
-                  </h4>
-                  <p>Developed and maintined external websites.</p>
+                  <h4 className="font-semibold">United Wholesale Mortgage</h4>
+                  <p className="text-sm">UI Developer</p>
+                </div>
+                <div>
+                  <h4 className="font-semibold">United Wholesale Mortgage</h4>
+                  <p className="text-sm">Quality Assurance Engineer</p>
+                </div>
+                <div>
+                  <h4 className="font-semibold">Advomas</h4>
+                  <p className="text-sm">IT Support Technician</p>
                 </div>
               </div>
             </div>
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,3 +1,4 @@
+import { BsGithub, BsGlobe } from "react-icons/bs";
 import RevealOnScroll from "../RevealOnScroll";
 
 const Projects = () => {
@@ -11,77 +12,91 @@ const Projects = () => {
             Featured Projects
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="p-6 rounded-xl border border-white/10">
-              <h3 className="text-xl font-bold mb-2">Interview Prep</h3>
-              <p className="text-gray-400 mb-4">
-                A job interview preparation platform that helps you practice
-                your interview skills with real-time feedback. Built with
-                Next.js for the user interface and backend logic, Firebase for
-                authentication and data storage, styled with TailwindCSS and
-                using Vapi AI voice agents with Google Gemini.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {[
-                  "Next.JS",
-                  "React",
-                  "Firebase",
-                  "Tailwind",
-                  "Vapi",
-                  "AI",
-                  "Google Gemini",
-                ].map((skill) => (
-                  <span
-                    key={skill}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                    {skill}
-                  </span>
-                ))}
-              </div>
+            <div className="p-6 rounded-xl border border-white/10 flex flex-col">
+              <div className="flex-1">
+                <h3 className="text-xl font-bold mb-2">Interview Prep</h3>
+                <p className="text-gray-400 mb-4">
+                  A job interview preparation platform that helps you practice
+                  your interview skills with real-time feedback. Built with
+                  Next.js for the user interface and backend logic, Firebase for
+                  authentication and data storage, styled with TailwindCSS and
+                  using Vapi AI voice agents with Google Gemini.
+                </p>
+                <div className="mt-auto">
+                  <div className="flex flex-wrap gap-2 mb-4">
+                    {[
+                      "AI",
+                      "Firebase",
+                      "Google Gemini",
+                      "Next.JS",
+                      "React",
+                      "shadcn/ui",
+                      "Tailwind",
+                      "TypeScript",
+                      "Vapi",
+                      "Zod",
+                    ].map((skill) => (
+                      <span
+                        key={skill}
+                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                        {skill}
+                      </span>
+                    ))}
+                  </div>
+                </div>
 
-              <div className="flex justify-between items-center">
-                <a
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                  target="_blank"
-                  href="https://mock-interview-lilac-omega.vercel.app/"
-                  rel="noreferrer">
-                  View Project{" "}
-                </a>
+                <div className="flex items-center gap-6 ml-2 ">
+                  <a
+                    className="icon"
+                    target="_blank"
+                    href="https://github.com/benstoica/mock-interview"
+                    rel="noreferrer">
+                    <BsGithub className="lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:cursor-pointer text-white/90 hover:text-white" />
+                  </a>
+                  <a
+                    className="icon"
+                    target="_blank"
+                    href="https://mock-interview-lilac-omega.vercel.app/"
+                    rel="noreferrer">
+                    <BsGlobe className="lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:cursor-pointer text-white/90 hover:text-white" />
+                  </a>
+                </div>
               </div>
             </div>
-            <div className="p-6 rounded-xl border border-white/10">
-              <h3 className="text-xl font-bold mb-2">Interview Prep</h3>
-              <p className="text-gray-400 mb-4">
-                A job interview preparation platform that helps you practice
-                your interview skills with real-time feedback. Built with
-                Next.js for the user interface and backend logic, Firebase for
-                authentication and data storage, styled with TailwindCSS and
-                using Vapi AI voice agents with Google Gemini.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {[
-                  "Next.JS",
-                  "React",
-                  "Firebase",
-                  "Tailwind",
-                  "Vapi",
-                  "AI",
-                  "Google Gemini",
-                ].map((skill) => (
-                  <span
-                    key={skill}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                    {skill}
-                  </span>
-                ))}
+            <div className="p-6 rounded-xl border border-white/10 flex flex-col">
+              <div className="flex-1">
+                <h3 className="text-xl font-bold mb-2">Evolve Gym</h3>
+                <p className="text-gray-400 mb-4">
+                  A new type of gym, one that will help you reach your goals
+                  like never before.
+                </p>
+              </div>
+              <div className="mt-auto">
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {["React", "Tailwind", "TypeScript", "Vite"].map((skill) => (
+                    <span
+                      key={skill}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                      {skill}
+                    </span>
+                  ))}
+                </div>
               </div>
 
-              <div className="flex justify-between items-center">
+              <div className="flex items-center gap-6 ml-2">
                 <a
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                  className="icon"
                   target="_blank"
-                  href="https://mock-interview-lilac-omega.vercel.app/"
+                  href="https://github.com/benstoica/gym-typescript"
                   rel="noreferrer">
-                  View Project{" "}
+                  <BsGithub className="lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:cursor-pointer text-white/90 hover:text-white" />
+                </a>
+                <a
+                  className="icon"
+                  target="_blank"
+                  href="https://gym-typescript-eb0.pages.dev/"
+                  rel="noreferrer">
+                  <BsGlobe className="lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:cursor-pointer text-white/90 hover:text-white" />
                 </a>
               </div>
             </div>
